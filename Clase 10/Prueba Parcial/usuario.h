dce36d929807b0e5166147bc8d0cc51f1e6bf047
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-04-24 19:02:15 -0300

    Clase 10

---
+++ Clase 10/Prueba Parcial/usuario.h
@@ -0,0 +1,14 @@
+#ifndef USUARIO_H_INCLUDED
+#define USUARIO_H_INCLUDED
+
+typedef struct
+    {
+        char* nombre;
+        char* apellido;
+        int usuarioId;
+        int isEmpty;
+    }Usuario;
+
+
+
+#endif // USUARIO_H_INCLUDED
